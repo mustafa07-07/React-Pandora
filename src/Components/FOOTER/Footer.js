616d00401758109f64c4bbb8d9bc8d5e4f90a277
mustafa07-07 <alanya879@gmail.com>
2021-11-19 17:46:02 +0300

    Updated Pages v4

--- src/Components/FOOTER/Footer.js
+++ src/Components/FOOTER/Footer.js
@@ -3,61 +3,47 @@ import {  Link } from "react-router-dom";
 export default class Footer extends Component {
     render() {
         return (
-            <footer className="footer-section" style={{background: "currentColor",paddingTop:80}}>
-		<div className="text-white mb-5">
-			<div className="row container-fluid ">
-		
-				<div className="col-lg-4 sm-1 footer-widget">
-					<h5 className="widget-title">Sources</h5>
-					<ul>
-					<li><Link exact="true" to="/">Home</Link></li>
-						<li><Link exact="true" to="/Seo">SEO</Link></li>
+            <footer class="bg-dark w-100 py-4 flex-shrink-0">
+				
+        <div class="container py-4">
+            <div class="row gy-4 gx-5">
+                <div class="col-lg-4 col-md-6">
+                    <h5 class="h1 text-white">Pandora</h5>
+                    <p class="small text-muted">We believe that each and every idea deserves a shot, and we will market your project real good.</p>
+                    <p class="small text-muted mb-0">&copy; Copyrights. All rights reserved. <a class="text-primary" href="#">agencypandora.com</a></p>
+                </div>
+                <div class="col-lg-2 col-md-6">
+                    <h5 class="text-white mb-3">Quick links</h5>
+                    <ul class="list-unstyled text-muted">
+					<li><Link exact="true" to="/Seo">SEO</Link></li>
 						<li><Link exact="true" to="/Influencer">Influencer</Link></li>
 						<li><Link exact="true" to="/Social">Social</Link></li>
 						<li><Link exact="true" to="/About">About Us</Link></li>
 						<li><Link exact="true" to="/Contact">Contact</Link></li>
-					</ul>
-				</div>
-				<div className="col-lg-4 sm-1 footer-widget">
-					<h5 className="widget-title">Follow us on social media</h5>
-					<div className="social d-flex justify-content-center">
+                    </ul>
+                </div>
+                <div class="col-lg-2 col-md-6">
+                    <h5 class="text-white mb-3">Social Media</h5>
+                    <div className="social row justify-content-center">
 						<a href="https://facebook.com"  rel="noreferrer" target="_blank" className="facebook"><i className="fa fa-facebook"></i></a>
 						<a href="https://instagram.com" rel="noreferrer" target="_blank" className="instagram"><i className="fa fa-instagram"></i></a>
 						<a href="https://twitter.com"   rel="noreferrer" target="_blank" className="twitter"><i className="fa fa-twitter"></i></a>
 						<a href="https://linkedin.com"   rel="noreferrer" target="_blank" className="linkedin"><i className="fa fa-linkedin"></i></a>
 					</div>
-				</div>
-				<div className="col-lg-4 sm-1 footer-widget">
-					<h5 className="widget-title">Subscribe to our Newsletter</h5>
-					
-				<div className="justify-content-center">
-					<div className="form-group">
-					<form class="d-block newsletter-form">
-						<input type="text "   placeholder="Enter your email"/>
-						<button>Get Started</button>
-					</form>
-					</div>
-			</div>			
-						
-				
-				</div>
-			
-			</div>
-			<div className="footer-bottom">
-				<div className="container row">
-					<div className="float-right">
-					
-					</div>
-					<div className="d-inline float-right p-2  text-white">
-						<ul className="footer-nav">
-							<li>Pandora 2021 </li>
-							<li><a href="mailto:support@pandora.com">support@pandora.com</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	</footer>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <h5 class="text-white mb-3">Newsletter</h5>
+                    <p class="small text-muted">Sign up for our social media newsletter to be informed about the innovations.</p>
+                    <form action="#">
+                        <div class="input-group mb-3">
+                            <input class="form-control" type="text" placeholder="Recipient's username"/>
+                            <button class="btn btn-primary" id="button-addon2" type="button"><i class="fas fa-paper-plane"></i></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </footer>
         )
     }
 }
