ba5ead22e6450d0679a2e798348f5adaa7318fcc
mustafa07-07 <alanya879@gmail.com>
2021-11-26 22:30:04 +0300

    addr up 2

--- src/Components/CONTACT/ContactAdress.js
+++ src/Components/CONTACT/ContactAdress.js
@@ -16,14 +16,16 @@ export default class ContactAdress extends Component {
                   <div class="info">
                     <h3 class="title">MAIL & WEBSITE</h3>
                     <p>
-                      <i class="fa fa-envelope" aria-hidden="true"></i>
+                    
                       <a href="mailto:info@agencypandora.com">
+					  <i class="fa fa-envelope" aria-hidden="true"></i>
                         info@agencypandora.com
+						
                       </a>
                       <br />
                      
-                      <i class="fa fa-globe" aria-hidden="true"></i>{" "}
                       <Link exact="true" to="/">
+					  <i class="fa fa-globe" aria-hidden="true"></i>
                         agencypandora.com
                       </Link>
                     
@@ -45,13 +47,14 @@ export default class ContactAdress extends Component {
                   <div class="info">
                     <h3 class="title">CONTACT</h3>
                     <p>
-                      <i class="fa fa-mobile" aria-hidden="true"></i>
                       <a
                         target="_blank"
                         rel="noreferrer"
 						
                         href="https://api.whatsapp.com/send?phone=31685289793&text=Hi Pandora"
                       >
+						                        <i class="fa fa-mobile" aria-hidden="true"></i>
+
                         (+31) 6 85289793
                         
                       </a>
@@ -72,8 +75,10 @@ export default class ContactAdress extends Component {
                   <div class="info">
                     <h3 class="title">ADDRESS</h3>	
                     <p>
+						<a target="_blank" rel="noreferrer" href="https://www.google.com/maps/place/Wilhelminakade+173,+3072+AP+Rotterdam,+Hollanda/data=!4m2!3m1!1s0x47c433657def0f47:0x7777299dea944681?sa=X&ved=2ahUKEwj-4JnZ2bb0AhVoh_0HHSE4BlsQ8gF6BAgWEAE">
                       <i class="fa fa-map-marker" aria-hidden="true"></i>
                       Wilhelminakade 173, 3072 AP Rotterdam, Netherlands
+					  </a>
                     </p>
                   </div>
                 </div>
