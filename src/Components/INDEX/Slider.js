48ad093dbd1bcfb9832be3f46c0616b0f87e0b39
mustafa07-07 <alanya879@gmail.com>
2021-12-13 17:56:31 +0300

    responsive partners

--- src/Components/INDEX/Slider.js
+++ src/Components/INDEX/Slider.js
@@ -42,6 +42,32 @@ export default class SliderArea extends Component {
       nextArrow: <NextArrow />,
       prevArrow: <PrevArrow />,
       slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     };
     return (
       <div className="container spad">
@@ -180,8 +206,8 @@ export default class SliderArea extends Component {
           <div>
             <a href="https://shibatoken.com/" 
             target="_blank"
-            rel="noreferrer"
-            >
+                        rel="noreferrer"
+>
               <img
                 height={150}
                 width={"auto"}
